5954fce04e475ffbb685224defa0e49181c048d1
nazarenomoreno <nazamoreno16@gmail.com>
2026-08-14 11:36:53 -0300

    include the images of every item

--- src/components/Product.tsx
+++ src/components/Product.tsx
@@ -1,12 +1,36 @@
 
+import type { Productype } from "../context/ProductsProvider";
+import type { ReducerActionType, ReducerAction } from "../context/CartProvider";
+import type { ReactElement } from "react";
+
+type PropsType = {
+  product: Productype;
+  dispatch: React.Dispatch<ReducerAction>;
+  REDUCER_ACTIONS: ReducerActionType;
+  inCart: boolean;
+};
+
+const Product = ({product, dispatch, REDUCER_ACTIONS, inCart}: PropsType ):ReactElement => {
 
 
-const Product = () => {
-  return (
-    <div>
+  const img: string = new URL(`../images/${product.sku}.jpg`, import.meta.url).href;
 
-    </div>
-  );
+  console.log(product.sku);
+  console.log(img);
+
+  const onAddToCart = ()=> dispatch({type: REDUCER_ACTIONS.ADD, payload: {...product, qty:1}})
+
+  const itemInCart = inCart ? '-> Item in cart' : null
+
+  const content = <article className="product">
+    <h3>{product.name}</h3>
+    <img src={img} alt={product.name} className="product-image" />
+    <p>{new Intl.NumberFormat('en-US', { style: 'currency', currency: 'USD' }).format(product.price)}{itemInCart}</p>
+    <button onClick={onAddToCart}>Add to Cart</button>
+  </article>
+
+
+  return content
 };
 
 export default Product;
